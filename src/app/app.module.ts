2b0106238c6eed165bc0a16b38bbcf2b549323b6
Vaibhav Sinha <vaibhavleoanto@gmail.com>
2020-05-27 02:27:15 +0530

    Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,17 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import 'hammerjs';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import  {DishService} from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +35,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule,
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
-
+import {Dish} from '../shared/dish';
+import {DishService} from '../services/dish.service';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -10,13 +9,15 @@ import { DISHES } from '../shared/dishes';
 
 export class MenuComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-  dishes :Dish[] = DISHES;
+  dishes :Dish[];
   selectedDish : Dish;
-   
+
+  constructor(private dishservice: DishService) { }
+  
+  ngOnInit() {
+    this.dishes = this.dishservice.getDishes();
+  }
+  
   onSelect(dish: Dish){
     this.selectedDish = dish;
   }
